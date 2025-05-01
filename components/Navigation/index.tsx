a683b32d68519b1893fea244cf1290b05b2d1ec7
Michael James Urian <businessmjurian@gmail.com>
2025-05-01 15:08:07 +0800

    chore: folder restructure

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -5,20 +5,23 @@ import { motion } from "motion/react";
 import { COLOR_THEMES, NAVIGATION_MENU } from "@/constants";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Palette } from "lucide-react";
+import { useActiveSection } from "@/hooks/useActiveSection";
+import { SectionId } from "@/providers/ActiveSectionProvider";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
-import { Palette } from "lucide-react";
-import { useTheme } from "@/hooks/useTheme";
-import { useActiveSection } from "@/hooks/useActiveSection";
-import { SectionId } from "@/providers/ActiveSectionProvider";
+} from "../ui/dropdown-menu";
 
 const Navigation = () => {
-  const { theme, setColorKey } = useTheme();
   const { activeSection, setActiveSection } = useActiveSection();
+
+  const handleColorKey = (colorKey: string) => {
+    const root = window.document.documentElement;
+    root.setAttribute("data-theme", colorKey);
+  };
 
   return (
     <header className="fixed top-0 left-0 right-0 z-40">
@@ -33,9 +36,9 @@ const Navigation = () => {
             <div className="flex items-center justify-between h-16 px-6">
               <Link
                 href="#hero"
-                className={`text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r ${theme.primary}`}
+                className={`text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary `}
               >
-                MJ<span className="text-zinc-200">Portfolio</span>
+                MJ<span className="text-white">Portfolio</span>
               </Link>
               <nav className="hidden sm:flex items-center space-x-1 pb-1 max-w-[500px] md:max-w-none">
                 {NAVIGATION_MENU.map((item) => (
@@ -48,8 +51,8 @@ const Navigation = () => {
                       <Button
                         className={`px-4 py-2 rounded-lg text-sm font-medium  ${
                           activeSection === item.id
-                            ? `bg-zinc-800/80 ${theme.accent}`
-                            : "text-zinc-400"
+                            ? `bg-zinc-800/80 text-primary`
+                            : "text-zinc-300"
                         }`}
                         variant="ghost"
                         onClick={() =>
@@ -82,7 +85,7 @@ const Navigation = () => {
                       <DropdownMenuItem
                         key={key}
                         onClick={() =>
-                          setColorKey(key as keyof typeof COLOR_THEMES)
+                          handleColorKey(key as keyof typeof COLOR_THEMES)
                         }
                         className="flex items-center gap-2 cursor-pointer"
                       >
