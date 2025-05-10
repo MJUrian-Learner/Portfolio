da7bb4ba2fb94fd74fab96d0838a1ccaea7de321
Michael James Urian <businessmjurian@gmail.com>
2025-05-10 19:10:35 +0800

    feat: enhance ScrollDownButton with animation for improved user engagement

--- components/ScrollDownButton.tsx
+++ components/ScrollDownButton.tsx
@@ -3,19 +3,26 @@ import { Button } from "./ui/button";
 import Link from "next/link";
 import { useActiveSection } from "@/hooks/useActiveSection";
 import { SectionId } from "@/providers/ActiveSectionProvider";
+import { motion } from "motion/react";
 
 const ScrollDownButton = ({ to, item }: { to: string; item: SectionId }) => {
   const { setActiveSection } = useActiveSection();
   return (
-    <Link href={to}>
-      <Button
-        variant="ghost"
-        className="text-primary hover:text-foreground/70"
-        onClick={() => setActiveSection(`${item as SectionId}`)}
-      >
-        Scroll Down
-      </Button>
-    </Link>
+    <motion.div
+      className="absolute bottom-10 left-1/2 -translate-x-1/2 hidden sm:block"
+      animate={{ y: [0, 10, 0] }}
+      transition={{ duration: 2, repeat: Number.POSITIVE_INFINITY }}
+    >
+      <Link href={to}>
+        <Button
+          variant="ghost"
+          className="text-primary hover:text-foreground/70"
+          onClick={() => setActiveSection(`${item as SectionId}`)}
+        >
+          Scroll Down
+        </Button>
+      </Link>
+    </motion.div>
   );
 };
 
